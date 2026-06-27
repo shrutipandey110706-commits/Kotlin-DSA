05a73ecc997620b6752f37e9acee4d3e2fdb7abd
Shruti Pandey <shrutipandey110706@gmail.com>
2026-06-27 21:31:54 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (92.96%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+
+        if (nums.isEmpty()) return 0 //
+
+        var i = 1
+
+        for (j in 1 until nums.size) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i] = nums[j]
+                i++
+            }
+        }
+
+        return i
+    }
+}
