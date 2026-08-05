479f00dbcf44f2775169d82ab49a945a3fc548e5
Shruti Pandey <shrutipandey110706@gmail.com>
2026-08-05 23:00:10 +0530

    Time: 73 ms (57.89%) | Memory: 38.5 MB (94.08%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.kt
@@ -0,0 +1,28 @@
+/** 
+ * The API guess is defined in the parent class.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * fun guess(num:Int):Int {}
+ */
+
+class Solution:GuessGame() {
+    override fun guessNumber(n:Int):Int {
+
+        var left = 1
+        var right = n
+        while (left<=right) {
+            val mid = left + (right -left )/2
+
+
+            when (guess(mid)) {
+
+                0-> return mid
+                -1->right = mid-1
+                1->left = mid +1
+            }
+        }
+        return -1
+    }
+}
